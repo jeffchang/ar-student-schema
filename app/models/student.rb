378b355ca79982dabec1316cb9ee43cdaa6ef490
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-31 15:30:19 -0700

    first ORM

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,14 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def age
+    ((Date.today - self.birthday) / 365).to_i
+  end
+
 end

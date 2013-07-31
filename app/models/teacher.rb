979fc07c89ac270709c46fb0e1757787bdd26647
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-31 15:59:24 -0700

    add Teacher model

---
+++ app/models/teacher.rb
@@ -0,0 +1,18 @@
+require_relative '../../db/config'
+require 'date'
+
+class Teacher < ActiveRecord::Base
+
+  validates_format_of :email, :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]{2,}\z/i, :message => "Invalid email address"
+  validates_uniqueness_of :email
+  validates_length_of :phone_digits, :minimum => 10
+
+  def phone_digits
+    self.phone.gsub(/\D/, "")
+  end
+
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+end

145f95437d015dc9f772a5c6af6901dee0002b03
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-31 16:30:19 -0700

    test for unique emails

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require 'date'
+require_relative '../app/models/teacher'
+
+describe Teacher, "validations" do
+
+  before(:all) do
+    raise RuntimeError, "be sure to run 'rake db:migrate' before running these specs" unless ActiveRecord::Base.connection.table_exists?(:teachers).should be_true
+    Teacher.delete_all
+  end
+
+  before(:each) do
+    @teacher = Teacher.new
+    @teacher.assign_attributes(
+      :first_name => "Kreay",
+      :last_name => "Shawn",
+      :email => 'kreayshawn@oaklandhiphop.net',
+      :phone => '(510) 555-1212 x4567'
+    )
+  end
+
+  it "shouldn't allow two teachers with the same email" do
+    another_teacher = Teacher.create!(
+      :email => @teacher.email,
+      :phone => @teacher.phone
+    )
+    @teacher.should_not be_valid
+  end
+
+end
